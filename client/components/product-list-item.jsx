1ba331e98066530cd7b0725ade9d53219f28a775
Brett Albright <balbright@learningfuze.com>
2020-10-06 20:30:54 +0000

    lunch time, work continues on productlist and productlistitem components

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function ProductListItem(props) {
-  
+  console.log(props)
+  const {image, name, price, shortDescription} = props.productInfo;
+
+  return (
+    <div className="card">
+      <img src={image} className="card-img-top" alt="A product image"/>
+      <div className="card-body">
+      <h5 className="card-title">{name}</h5>
+      <p className="card-text">{`$${price.toString().slice(0,2)}`}</p>
+      </div>
+    </div>
+  )
 }
--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import ProductListItem from "./product-list-item";
 
 export default class ProductList extends React.Component {
   constructor(props) {
@@ -8,6 +8,30 @@ export default class ProductList extends React.Component {
     this.state = {
       products: []
     };
+
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    const options = { method: "GET" };
+    fetch("/api/products", options)
+    .then(resp => resp.json())
+    .then(resp => this.setState({ products: resp }));
+  }
+
+  render() {
+    if (!this.state.products.length) {
+      return <h1>No products available</h1>;
+    } else {
+      return (
+        <section className="d-flex justify-content-center align-items-center">
+          {this.state.products.map( product => <ProductListItem key={product.productId} productInfo={product} />)}
+        </section>
+      );
+    }
 
   }
 
