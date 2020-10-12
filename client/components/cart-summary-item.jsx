4b543e5df420991450780fd54d80eab059fa39c2
Brett Albright <balbright@learningfuze.com>
2020-10-12 22:54:40 +0000

    completed implementation of cart summary and cart summary item components including: completed view functionality to show cart view on click of cart icon. completed cart summary display per figma. Completed total price display. Completed cart summary item display to match figma.

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import priceFormatter from '../price-format';
 
 export default function CartSummaryItem(props) {
-  console.log(props);
+  const { image, name, price, shortDescription } = props.cartData;
   return (
-    <div className="col-12">
-      <img className="col-4" src="" alt=""/>
-      <div className="col-8">
-        <h3 className="cart-title"></h3>
+    <div className="col-12 card flex-row shadow-sm align-items-center mb-4">
+      <img className="col-4 product-details-image p-3" src={image} alt=""/>
+      <div className="col-8 card-body">
+        <h3 className="card-title">{name}</h3>
+        <p className="card-text">{priceFormatter(price)}</p>
+        <p className="card-text">{shortDescription}</p>
       </div>
-
     </div>
   );
 }
