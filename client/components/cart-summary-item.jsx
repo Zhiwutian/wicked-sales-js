38ce482f95263ae852d6569f859d135380edc7e7
Brett Albright <balbright@learningfuze.com>
2020-10-12 20:29:09 +0000

    completed iniital build of cartsummary, added new view option for cart in app.js, begun build of cart summary item

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React from 'react';
 
 export default function CartSummaryItem(props) {
-  return null
+  console.log(props);
+  return (
+    <div className="col-12">
+      <img className="col-4" src="" alt=""/>
+      <div className="col-8">
+        <h3 className="cart-title"></h3>
+      </div>
+
+    </div>
+  );
 }
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -1,5 +1,15 @@
-import React from "react";
-
+import React from 'react';
+import CartSummaryItem from './cart-summary-item';
 export default function CartSummary(props) {
-  
+
+  return (
+    <div className="">
+      <button onClick={() => props.setView('catalog', {})}className="btn">&#8592; Back to Catalog</button>
+      <h1>My Cart</h1>
+      <div className="row">
+        <CartSummaryItem cartData={props.cartState}/>
+      </div>
+      <h2>Item Total <span></span></h2>
+    </div>
+  );
 }
